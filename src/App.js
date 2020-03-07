0745f4e429de3ae5c82f55c8c90b463eb59f65c2
merry-sunwoo <sunwoojung517@gmail.com>
2020-03-07 08:35:41 +0000

    feat: Adjust props concept

--- src/App.js
+++ src/App.js
@@ -1,20 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+      {/*        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>*/}
+        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>                
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>                
+    )
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    )
   }
 }
 
