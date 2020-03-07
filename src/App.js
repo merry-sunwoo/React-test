cdb54e9542dbc01a841eb768c07041252afb8d44
merry-sunwoo <sunwoojung517@gmail.com>
2020-03-07 14:45:39 +0000

    feat: Adjust state concept

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {title:"Web", sub:"World Wide Web"},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML is for information"},
+        {id:2, title:"CSS", desc:"CSS is for design"},
+        {id:3, title:"JavaScript", desc:"JavaScript is for interaction"},
+        {id:4, title:"Node", desc:"Node is for server-side development"}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,8 +28,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>*/}
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
-        <TOC></TOC>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language"></Content>
       </div>
     );
